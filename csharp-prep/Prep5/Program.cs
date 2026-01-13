8c0bd5ea1e2734f7aeeecb87d8207bf1281d7e10
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-01-13 10:11:28 -0700

    completed prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Initialize variables for later use
+        string name;
+        double number;
+        int year;
+
+        // Call all of the functions
+        DisplayWelcome();
+        name = PromptUserName();
+        number = PromptUserNumber();
+        year = PromptUserBirthYear();
+        DisplayResult(name, SquareNumber(number), year);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static double PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return double.Parse(Console.ReadLine());
+    }
+
+    static int PromptUserBirthYear()
+    {
+        Console.Write("Please enter the year you were born: ");
+        return int.Parse(Console.ReadLine());   
+    }
+
+    static double SquareNumber(double number)
+    {
+        return Math.Pow(number, 2);
+    }
+
+    static void DisplayResult(string name, double number, int year)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+        Console.WriteLine($"{name}, you will turn {2026 - year} this year.");
     }
 }
