ec77137b3cc8142433a0ea11ffe078ff0c941b3c
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-02-17 16:19:49 -0700

    fix typo

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,6 +1,6 @@
 public class BreathingActivity : Activity
 {
-    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.") {}
+    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.") {}
 
     public void Run()
     {
