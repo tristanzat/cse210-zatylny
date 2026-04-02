d86c435e39e02b31a2e37e6b5e3a69530fe49b41
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-04-02 10:20:09 -0600

    get AI to fill in moves with TODO comments for missing functionality

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -34,19 +34,35 @@ class Program
             ],
             // Mewtwo
             [
-                
+                // Gen IX learnset picks (mostly damage)
+                new DamageMove("Psychic", PokemonType.Psychic, 10, 90, 100, 1),
+                new DamageMove("Aura Sphere", PokemonType.Fighting, 20, 80, 100, 1),
+                new DamageMove("Ice Beam", PokemonType.Ice, 10, 90, 100, 1),
+                new DamageMove("Shadow Ball", PokemonType.Ghost, 15, 80, 100, 1)
             ],
             // Gardevoir
             [
-                
+                // Gen IX learnset picks (mostly damage)
+                new DamageMove("Moonblast", PokemonType.Fairy, 15, 95, 100, 1),
+                new DamageMove("Psychic", PokemonType.Psychic, 10, 90, 100, 1),
+                new DamageMove("Dazzling Gleam", PokemonType.Fairy, 10, 80, 100, 1),
+                new DamageMove("Shadow Ball", PokemonType.Ghost, 15, 80, 100, 1)
             ],
             // Lucario
             [
-                
+                // Gen IX learnset picks (mostly damage)
+                new DamageMove("Extreme Speed", PokemonType.Normal, 5, 80, 100, 0, 2),
+                new DamageMove("Bullet Punch", PokemonType.Steel, 30, 40, 100, 0, 1),
+                new DamageMove("Dragon Pulse", PokemonType.Dragon, 10, 85, 100, 1),
+                new DamageMove("Earthquake", PokemonType.Ground, 10, 100, 100, 0)
             ],
             // Rayquaza
             [
-                
+                // Gen IX learnset picks (mostly damage)
+                new DamageMove("Dragon Claw", PokemonType.Dragon, 15, 80, 100, 0),
+                new DamageMove("Earthquake", PokemonType.Ground, 10, 100, 100, 0),
+                new DamageMove("Crunch", PokemonType.Dark, 15, 80, 100, 0),
+                new DamageMove("Extreme Speed", PokemonType.Normal, 5, 80, 100, 0, 2)
             ],
             // Hawlucha
             [
@@ -57,23 +73,60 @@ class Program
             ],
             // Tyrantrum
             [
-
+                // TODO (Bulbapedia Head Smash): implement recoil damage (1/2 of damage dealt to target).
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Head_Smash_(move)
+                new DamageMove("Head Smash", PokemonType.Rock, 5, 150, 80, 0),
+                new DamageMove("Earthquake", PokemonType.Ground, 10, 100, 100, 0),
+                new DamageMove("Dragon Claw", PokemonType.Dragon, 15, 80, 100, 0),
+                // TODO (Bulbapedia Crunch): implement 20% chance to lower target's Defense by 1 stage.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Crunch_(move)
+                new DamageMove("Crunch", PokemonType.Dark, 15, 80, 100, 0)
             ],
             // Aurorus
             [
-
+                new StatusMove("Thunder", PokemonType.Electric, 10, 110, 70, 1, [new Paralysis()], [30]),
+                new StatusMove("Blizzard", PokemonType.Ice, 5, 110, 70, 1, [new Freeze()], [10]),
+                // TODO (Bulbapedia Light Screen): implement side condition reducing special move damage for 5 turns.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Light_Screen_(move)
+                new StatMove("Light Screen", PokemonType.Psychic, 30, 0, 100, 2, [3], [1], [true]),
+                // TODO (Bulbapedia Reflect): implement side condition reducing physical move damage for 5 turns.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Reflect_(move)
+                new StatMove("Reflect", PokemonType.Psychic, 20, 0, 100, 2, [1], [1], [true])
             ],
             // Gourgeist
             [
-
+                // TODO (Bulbapedia Trick-or-Treat): implement adding Ghost type to target's type list.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Trick-or-Treat_(move)
+                new StatMove("Trick-or-Treat", PokemonType.Ghost, 20, 0, 100, 2, [6], [0], [false]),
+                // TODO (Bulbapedia Phantom Force): implement two-turn semi-invulnerable charging behavior.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Phantom_Force_(move)
+                new DamageMove("Phantom Force", PokemonType.Ghost, 10, 90, 100, 0),
+                new DamageMove("Seed Bomb", PokemonType.Grass, 15, 80, 100, 0),
+                new DamageMove("Shadow Sneak", PokemonType.Ghost, 30, 40, 100, 0, 1)
             ],
             // Goodra
             [
-
+                new DamageMove("Dragon Pulse", PokemonType.Dragon, 10, 85, 100, 1),
+                // TODO (Bulbapedia Muddy Water): implement 30% chance to lower target accuracy by 1 stage.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Muddy_Water_(move)
+                new DamageMove("Muddy Water", PokemonType.Water, 10, 90, 85, 1),
+                new StatusMove("Fire Blast", PokemonType.Fire, 5, 110, 85, 1, [new Burn()], [10]),
+                // TODO (Bulbapedia Focus Blast): implement 10% chance to lower target Sp. Def by 1 stage.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Focus_Blast_(move)
+                new DamageMove("Focus Blast", PokemonType.Fighting, 5, 120, 70, 1)
             ],
             // Gardevoir
             [
-                
+                // TODO (Bulbapedia Moonblast): implement 30% chance to lower target Sp. Atk by 1 stage.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Moonblast_(move)
+                new DamageMove("Moonblast", PokemonType.Fairy, 15, 95, 100, 1),
+                // TODO (Bulbapedia Psychic): implement 10% chance to lower target Sp. Def by 1 stage.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Psychic_(move)
+                new DamageMove("Psychic", PokemonType.Psychic, 10, 90, 100, 1),
+                // TODO (Bulbapedia Shadow Ball): implement 20% chance to lower target Sp. Def by 1 stage.
+                // Source: https://bulbapedia.bulbagarden.net/wiki/Shadow_Ball_(move)
+                new DamageMove("Shadow Ball", PokemonType.Ghost, 15, 80, 100, 1),
+                new StatusMove("Thunderbolt", PokemonType.Electric, 15, 90, 100, 1, [new Paralysis()], [10])
             ]
             ];
         List<Pokemon> userTeam = [];
